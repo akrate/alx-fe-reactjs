171fa2ecc67f01bae3d02fc7cd517faa37148bc3
Akrate Oussama <aoussama@c1r5p5.1337.ma>
2025-11-07 16:07:17 +0100

    test

---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: 'black', padding: '20px', textAlign: 'center' }}>
+      <h2>Main Content Area</h2>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
