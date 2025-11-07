eae1e83bb5fddd905f578b1f36f8bd56e12d13ef
Akrate Oussama <aoussama@c1r5p5.1337.ma>
2025-11-07 14:11:04 +0100

    test

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header'
+import UserProfile from './components/UserProfile'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Header />
+        <UserProfile name="oussama akrate" age={23} bio="I love traveling and discovering new cities." />
+        <MainContent />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: 'black', padding: '20px', textAlign: 'center' }}>
+      <h2>Main Content Area</h2>
+      <p>Here you can display interesting information about cities.</p>
+    </main>
+  );
+}
+
+export default MainContent;
