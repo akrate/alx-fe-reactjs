c72e9723001bb6c5ae528d3dc883e9067aebc74b
Akrate Oussama <aoussama@c1r6p3.1337.ma>
2025-11-16 02:02:49 +0100

    test

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,27 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
-import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div style={{ maxWidth: '600px', margin: '0 auto' }}>
-      <h1>Recipe Sharing App</h1>
+    <BrowserRouter>
+      <div style={{ maxWidth: '600px', margin: '0 auto' }}>
+        <h1>Recipe Sharing App</h1>
 
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          }
-        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
 
-        <Route path="/recipes/:id" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
