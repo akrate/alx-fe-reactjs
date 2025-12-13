bad333498051546bee5cbd6f8511cba02222c8a0
akrate <oussamaakrate590@gmail.com>
2025-12-13 15:56:08 +0100

    React Query into a React project to fetch

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -2,15 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PostsComponent from "./components/PostsComponent";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>
-      <h1>React Query Demo</h1>
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 }
 
 export default App;
+
